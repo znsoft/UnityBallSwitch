09bf90cf59f35892da5a3e7ecb316655fb3da7cd
znsoft <zn@bk.ru>
2015-10-01 12:19:29 +1100

    Испытание звуков при коллизии

---
+++ Assets/Scripts/TouchPlate.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class TouchPlate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		AudioSource touchSound = col.gameObject.GetComponent<AudioSource> ();
+		if(touchSound!=null)touchSound.Play ();
+		//Debug.Log ("PlaySound");
+		//if(col.gameObject.name == "prop_powerCube")		Destroy(col.gameObject);
+		//this.Play()
+	}
+}
