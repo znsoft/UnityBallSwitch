af7a2a9b5646c9e7f5bb09a90cc9d767e0c70f80
znsoft <zn@bk.ru>
2015-10-14 02:11:57 +1100

    Блоки генерируются

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -7,14 +7,47 @@ public class Buttons : MonoBehaviour {
 	public GameObject botBalls;
 	GameObject playerBalls;
 	public GameObject myCamera;
+	Rigidbody botRigidBody;
+	public GameObject[] prefabs;
+
 	// Use this for initialization
 	IEnumerator	 Start() {
 		botBalls.SendMessage ("SetBotBall", true);
 		botBalls.SendMessage ("SetPlayerBall", "Paper");
+		botRigidBody = botBalls.GetComponent<Rigidbody> ();
 		yield return new WaitForSeconds(1);
 		playerBalls = Instantiate (playerBall);
 		myCamera.GetComponent<AutoCam> ().SetTarget( playerBalls.transform);
+		this.StartCoroutine ("RepeatAction", botBalls);
+		Random.seed = 0;
 	}
+
+
+
+
+IEnumerator RepeatAction ( GameObject botBall)
+	{
+		//------------
+		Vector3 botVelocity = botRigidBody.velocity;
+		var gravity = Physics.gravity;
+		float t = 1;//forecast time
+		Vector3 futurePosition = botVelocity * t + (gravity * t * t) / 2;
+		//Debug.DrawRay (transform.position, futurePosition, Color.red);
+		//GenerateBlock(botBalls,
+		if (botVelocity.magnitude > 7.0F) {
+			GameObject block = Instantiate<GameObject> (prefabs [Random.Range(0,prefabs.GetUpperBound(0))]);
+			block.transform.position = botBall.transform.position + futurePosition;
+			block.transform.rotation = Random.rotation;
+			block.transform.rotation.SetLookRotation (botBall.transform.position);
+		}
+		//===========
+		yield return new WaitForSeconds (Random.Range(1.5F ,3));
+		this.StartCoroutine ("RepeatAction", botBall);
+	}
+
+
+
+
 
 	// Update is called once per frame
 	void Update () {
