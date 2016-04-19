e900de93e7c4ab28968c55cd99e15d25c7b7c281
znsoft <zn@bk.ru>
2016-04-19 13:49:49 +1000

    Добавил линию полета в gameView

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,13 +8,20 @@ public class PlayerControl : MonoBehaviour {
 	private Vector3 m_CamForward;             // The current forward direction of the camera
 	private Vector3 m_Move;
 	private bool m_Jump, pause = false;                      // the world-relative desired move direction, calculated from the camForward and user input.
-	public Rigidbody myRigidBody;
-	
+	public GameObject myBody;
+	Rigidbody myRigidBody;
+	//GameObject line;
+	LineRenderer line;
+
 	private void Start()
 	{
-	//	myRigidBody = GetComponent<Rigidbody> ();
+		line = GetComponent<LineRenderer> ();
+		line.enabled = false;
+		myRigidBody = myBody.GetComponent<Rigidbody> ();
 	}
-	
+
+
+
 	
 	private void Update()
 	{
@@ -22,12 +29,29 @@ public class PlayerControl : MonoBehaviour {
 		if (m_Jump)
 			pause = !pause;
 		Time.timeScale = pause ? 0 : 1;
-
+		line.enabled = pause;
+		if (pause) {
+			DrawWay();
+		
+		}
 		if (Input.GetKeyDown (KeyCode.Escape))
 			Application.Quit ();
 	}
 
 
+
+
+	void DrawWay(){
+
+		Vector3 myVelocity = myRigidBody.velocity;
+		var gravity = Physics.gravity;
+		float t = 1;//forecast time
+		Vector3 futurePosition = myVelocity * t + (gravity * t * t) / 2;
+		line.SetPosition (0, myBody.transform.position);
+		line.SetPosition (1, myBody.transform.position + futurePosition);
+		//Debug.DrawRay (transform.position, futurePosition, Color.red);
+	
+	}
 	
 	// Fixed update is called in sync with physics
 	private void FixedUpdate()
